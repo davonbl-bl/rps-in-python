49f603ba743cb33e9c9e9c675fad7150174804c9
davonbl-bl <davonbl@gmail.com>
2023-06-07 14:23:51 -0400

    adding and editing functions

--- rps.py
+++ rps.py
@@ -1,26 +1,28 @@
 import random
 import math
 
-def howManyGames():
-    try:
-        numberOfGames = input('How many games do you want to play up to?\n')
-        convertToInt = int(numberOfGames)
-        # if not math.isnan(numberOfGames) and (type(numberOfGames) is int):
-        # if convertToInt % 1 != 0:
-        #     if isinstance(convertToInt, int):
-        print(type(convertToInt))
-        print(f'Okay great! {convertToInt} it is!')
-        return convertToInt
-    # else:
-    except:
-        print('What you wrote is not a number.')
-        getAnswer = str(input('Do you want to try again? (yes/no)\n')).lower()
-        if(getAnswer == 'yes'):
-            howManyGames()
-        else:
-            print('Okay, have a good day')
+playerScore = 0
+cpuScore = 0
+# def howManyGames():
+#     try:
+#         numberOfGames = input('How many games do you want to play up to?\n')
+#         convertToInt = int(numberOfGames)
+#         # if not math.isnan(numberOfGames) and (type(numberOfGames) is int):
+#         # if convertToInt % 1 != 0:
+#         #     if isinstance(convertToInt, int):
+#         # print('first: ', type(convertToInt))
+#         print(f'Okay great! {convertToInt} it is!')
+#         return convertToInt
+#     # else:
+#     except ValueError:
+#         print('What you wrote is not a number.')
+#         getAnswer = str(input('Do you want to try again? (yes/no)\n')).lower()
+#         if(getAnswer == 'yes'):
+#             howManyGames()
+#         else:
+#             print('Okay, have a good day')
 
-howManyGames()
+# howManyGames()
 
 # def howManyGames():
 #         numberOfGames = input('How many games do you want to play up to?\n')
@@ -49,28 +51,48 @@ def computerChoice():
     print(type(cpuChooses))
     return cpuChooses
 
-print(computerChoice())
+# print(computerChoice())
 
-# def playerChoice():
-#     playerChooses = int(input(f'What is your choice? \n 1 for Rock \n 2 for Paper or \n 3 for Scissors\n'))
-#     # print(type(playerChooses))
-#     if(playerChooses == 1):
-#         return 'rock'
-#     elif(playerChooses == 2):
-#         return 'paper'
-#     elif(playerChooses == 3):
-#         return 'scissors'
-#     else:
-#         print('Wrong number and/or Key')
-#         getAnswer = str(input('Do you want to try again? (Write yes/no as a your response)\n').lower()).strip()
-#         if(getAnswer == 'yes'):
-#             print(playerChoice())
-#         else:
-#             print('Have a good day.')
+def playerChoice():
+    playerChooses = int(input(f'What is your choice? \n 1 for Rock \n 2 for Paper or \n 3 for Scissors\n'))
+    # print(type(playerChooses))
+    if(playerChooses == 1):
+        return 'rock'
+    elif(playerChooses == 2):
+        return 'paper'
+    elif(playerChooses == 3):
+        return 'scissors'
+    else:
+        print('Wrong number and/or Key')
+        getAnswer = str(input('Do you want to try again? (Write yes/no as a your response)\n').lower()).strip()
+        if(getAnswer == 'yes'):
+            print(playerChoice())
+        else:
+            print('Have a good day.')
         
     
-# print(playerChoice())
+def letsPlayAGame():
+    player = playerChoice()
+    cpu = computerChoice()
+    if(player == 'rock' and cpu == 'scissors' or player == 'scissors' and cpu == 'paper' or player == 'paper' and cpu == 'rock' ):
+        print(f'Player chooses: {player}\n CPU chooses: {cpu}')
+        print('Player wins')
+        playerScore = playerScore + 1
+        print(f'The score is ...\n Player: {playerScore} \n CPU: {cpuScore}')
+        whoWins(playerScore)
+        # write your return for playerScore to see who won the whole game 
+    elif(player == cpu):
+        print('It\'s a tie')
+        playerChoice()
+    else:
+        print(f'Player chooses: {player}\n CPU chooses: {cpu}')
+        print('CPU wins')
+        cpuScore = cpuScore + 1
+        print(f'The score is ...\n Player: {playerScore} \n CPU: {cpuScore}')
+        whoWins(cpuScore)
 
+def whoWins(currentScore):
+    numOfGames = howManyGames()
 
     
         
