cad31b9a0170001d9f794959490ebed0bf069eb4
davonbl-bl <davonbl@gmail.com>
2023-06-07 19:55:55 -0400

    refactor code to make it user friendly

--- rps.py
+++ rps.py
@@ -9,15 +9,12 @@ def computerChoice():
     testing = round(random.random() * 2)
     cpuOptions = ['rock', 'paper', 'scissors']
     cpuChooses = cpuOptions[testing]
-    # print(type(cpuChooses))
     return cpuChooses
 
-# print(computerChoice())
 
 def playerChoice(numOfGames):
     countGames = numOfGames
     playerChooses = int(input(f'What is your choice? \n 1 for Rock \n 2 for Paper or \n 3 for Scissors\n'))
-    # print(type(playerChooses))
     if(playerChooses == 1):
         letsPlayAGame('rock', countGames)
     elif(playerChooses == 2):
@@ -40,15 +37,25 @@ def whoWins(playerPoints, cpuPoints, numOfGames):
     cpu = cpuPoints
 
     if player == countGames:
-        print('player wins the game!')
+        print('player wins the game!\n')
         playerScore = 0
         cpuScore = 0
+        getAnswer = str(input('Do you want to play again? (yes/no)\n')).lower
+        if getAnswer == 'yes':
+            howManyGames()
+        else:
+            sys.exit()
     elif cpu == countGames:
-        print('cpu wins the game')
+        print('cpu wins the game!\n')
         playerScore = 0
         cpuScore = 0
+        getAnswer = str(input('Do you want to play again? (yes/no)\n')).lower()
+        if getAnswer == 'yes':
+            howManyGames()
+        else:
+            sys.exit()
     else:
-        print('the game continues...')
+        print('the game continues...\n')
         playerChoice(countGames)
         
     
@@ -58,22 +65,24 @@ def letsPlayAGame(ply, numOfGames):
     player = ply
     countGames = numOfGames
     cpu = computerChoice()
+    print('')
     if(player == 'rock' and cpu == 'scissors' or player == 'scissors' and cpu == 'paper' or player == 'paper' and cpu == 'rock' ):
         playerScore += 1
-        print(f'Player chooses: {player}\n CPU chooses: {cpu}')
+        print(f'Player chooses: {player}\nCPU chooses: {cpu}\n')
+        # print('')
         print('Player wins')
 
-        print(f'The score is ...\n Player: {playerScore} \n CPU: {cpuScore}')
+        print(f'\nThe score is ...\n Player: {playerScore} \n CPU: {cpuScore}\n')
         whoWins(playerScore, cpuScore, countGames)
-# write your return for playerScore to see who won the whole game 
     elif(player == cpu):
-        print('It\'s a tie')
+        print('It\'s a tie\n')
         playerChoice(countGames)
     else:
         cpuScore += 1
-        print(f'Player chooses: {player}\n CPU chooses: {cpu}')
+        print(f'Player chooses: {player}\n CPU chooses: {cpu}\n')
+        # print('')
         print('CPU wins')
-        print(f'The score is ...\n Player: {playerScore} \n CPU: {cpuScore}')
+        print(f'The score is ...\n Player: {playerScore} \n CPU:{cpuScore}\n')
         whoWins(playerScore, cpuScore, countGames)
 
 
@@ -81,48 +90,35 @@ def letsPlayAGame(ply, numOfGames):
 def howManyGames():
     try:
         numberOfGames = int(input('How many games do you want to play up to?\n'))
-        # convertToInt = int(numberOfGames)
-# if not math.isnan(numberOfGames) and (type(numberOfGames) is int):
-# if convertToInt % 1 != 0:
-# if isinstance(convertToInt, int):
-# print('first: ', type(convertToInt))
+        print('')
         print(f'Okay great! {numberOfGames} it is!')
         print('')
-        startGame = input('Do you want to start the Game? (yes/no)\n').lower()
-        if(startGame == 'yes'):
-            playerChoice(numberOfGames)
-        else:
-            toEnd = input('Are you sure? (yes/no) \n')
-            if(toEnd == 'yes'):
-                sys.exit()
-    # else:
+        playerChoice(numberOfGames)
+        # startGame = input('Start the Game? (yes/no)\n').lower()
+        # if(startGame == 'yes'):
+        #     print('')
+        #     playerChoice(numberOfGames)
+        # elif(startGame == 'no'):
+        #     toEnd = input('Are you sure? (yes/no) \n')
+        #     if(toEnd == 'yes'):
+        #         sys.exit()
+        #     else:
+        #         print('')
+        # else:
     except ValueError:
+        print('')
         print('What you wrote is not a number.')
+        print('')
         getAnswer = str(input('Do you want to try again? (yes/no)\n')).lower()
         if(getAnswer == 'yes'):
+            print('')
             howManyGames()
         else:
+            print('')
             print('Okay, have a good day')
 howManyGames()
 
-# def howManyGames():
-#         numberOfGames = input('How many games do you want to play up to?\n')
-#         convertToInt = float(numberOfGames)
-#         if not math.isnan(numberOfGames) and (type(numberOfGames) is float):
-#         # if convertToInt % 1 != 0:
-#         #     if isinstance(convertToInt, int):
-#                 print(type(convertToInt))
-#                 print(f'Okay great! {convertToInt} it is!')
-#                 return convertToInt
-#         else:
-#             print('What you wrote is not a number.')
-#             getAnswer = str(input('Do you want to try again? (yes/no)')).lower()
-#         if(getAnswer == 'yes'):
-#             howManyGames()
-#         else:
-#             print('Okay, have a good day')
 
-# howManyGames()
 
 
 
